c48561450f4104e44238b6c3488d5f8aaac26fcc
Christopher Perez <Data.Christopher.Perez@Gmail.com>
2026-07-29 16:40:06 -0400

    updated config.py and created gitignore at the root level

---
+++ Backend/config.py
@@ -0,0 +1,22 @@
+
+import os
+from dotenv import load_dotenv
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from flask_restful import Api
+from flask_cors import CORS
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["AQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = os.environ.get("SECRET_KEY", "F.A.S.T.-Library-Secret")
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+bcrypt = Bcrypt(app)
+api = Api(app)
+CORS(app, supports_credentials=True)
